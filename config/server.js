bd6bc91db60e9837c81bf3407e6c2847be2826b6
André <andrevictor50@gmail.com>
2021-02-03 20:44:42 -0300

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a8950725b1bcfd0e3bf4aae39bab8c40'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6178750b-b751-45ff-b2a0-ff7fd4393d84'
+};
